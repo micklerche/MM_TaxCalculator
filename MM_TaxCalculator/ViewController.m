66fe2138bed101773742fe540d67bb6cb4939d61
GrumpyDane <micklerche@gmail.com>
2015-02-09 18:16:32 -0600

    Initial commit

--- MM_TaxCalculator/ViewController.m
+++ MM_TaxCalculator/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (strong, nonatomic) IBOutlet UITextField *priceTextField;
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
 
 @end
 
@@ -16,7 +23,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.caTax = 7.5;
+    self.chiTax = 9.25;
+    self.nyTax = 4.5;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +33,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onCalculateButtonTapped:(id)sender {
+
+    double x = [self.priceTextField.text doubleValue];
+    double y = self.segmentedControl.selectedSegmentIndex == 0 ? self.caTax :
+    self.segmentedControl.selectedSegmentIndex == 1 ? self.chiTax : self.nyTax;
+
+    self.resultLabel.text = [NSString stringWithFormat:@"%.02f", x*y*0.01];
+}
 @end
